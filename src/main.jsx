0748a8df7e3c8215d7cd2714ff3f38a21f08bef0
humanwade <seounghoon.wade@gmail.com>
2025-09-27 16:09:36 -0400

    update main jsx to open in other PC

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+const basename = import.meta.env.MODE === "production" ? "/portfolio" : "/";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/portfolio">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>
